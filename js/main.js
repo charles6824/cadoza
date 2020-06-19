077647c0168e7f64fd90fc9b6a093ecd85dd41f2
Charles Ekom charles <charlescharlesy@gmail.com>
2020-06-19 21:29:38 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $("#product-slider").owlCarousel({
+        items:3,
+        itemsDesktop:[1199,2],
+        itemsDesktopSmall:[1000,2],
+        itemsMobile:[700,1],
+        pagination:false,
+        navigationText:false,
+        autoPlay:true
+    });
+});
